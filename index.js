63a999022584d3f8ff7a469ad58722947036518a
salescommanders <91628298+salescommanders@users.noreply.github.com>
2022-02-19 21:19:38 +0600

    Calculator Done

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+function updateClock(){
+    var now = new Date();
+    var hou = now.getHours(),
+        min = now.getMinutes(),
+        pe = "AM";
+
+    if (hou == 0) {
+        hou = 12;
+    } 
+    if(hou > 12 ) {
+        hou = hou - 12;
+        pe = 'PM';
+    }
+    Number.prototype.pad = function(digit){
+        for (var n = this.toString();n.length < digit; n = 0 + n );
+        return n;
+ 
+    }
+
+     var ids = ["hours", "minutes", "period"];
+     var values = [hou.pad(2),min.pad(2), pe ];
+     for (let i = 0; i < ids.length; i++)
+     document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
+function initClock() {
+    updateClock();
+    window.setInterval("updateClock()",1);
+}
+
+// Total Counter
 let display = document.getElementById('display');
 buttons = document.querySelectorAll('button');
 let displayValue = '';
@@ -25,3 +55,4 @@ for(item of buttons){
         }
     })
 }
+
